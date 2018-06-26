1968dd3bd78915d9ebf08e7f26eb7ba6c8ef3333
Raul <rdavilar7@gmail.com>
2018-06-26 22:38:14 +0200

    Final version

--- main.py
+++ main.py
@@ -5,6 +5,11 @@ import warnings
 from distutils.version import LooseVersion
 import project_tests as tests
 
+import scipy.misc
+from moviepy.editor import VideoFileClip
+from IPython.display import HTML
+import numpy as np
+import cv2
 
 # Check TensorFlow Version
 assert LooseVersion(tf.__version__) >= LooseVersion('1.0'), 'Please use TensorFlow version 1.0 or newer.  You are using {}'.format(tf.__version__)
@@ -16,6 +21,15 @@ if not tf.test.gpu_device_name():
 else:
     print('Default GPU Device: {}'.format(tf.test.gpu_device_name()))
 
+# Define session as global
+sess = tf.Session()
+# Define logits tensor as global
+logits = tf.zeros([160*576,2])
+# Define global placeholders
+keep_prob = tf.placeholder(tf.float32)
+input_image = tf.placeholder(tf.float32, (None, None, None, 3))
+# Image shape
+image_shape = (160, 576)
 
 def load_vgg(sess, vgg_path):
     """
@@ -24,8 +38,8 @@ def load_vgg(sess, vgg_path):
     :param vgg_path: Path to vgg folder, containing "variables/" and "saved_model.pb"
     :return: Tuple of Tensors from VGG model (image_input, keep_prob, layer3_out, layer4_out, layer7_out)
     """
-    # TODO: Implement function
-    #   Use tf.saved_model.loader.load to load the model and weights
+
+    # Names of tensors to be extracted from network
     vgg_tag = 'vgg16'
     vgg_input_tensor_name = 'image_input:0'
     vgg_keep_prob_tensor_name = 'keep_prob:0'
@@ -33,7 +47,22 @@ def load_vgg(sess, vgg_path):
     vgg_layer4_out_tensor_name = 'layer4_out:0'
     vgg_layer7_out_tensor_name = 'layer7_out:0'
     
-    return None, None, None, None, None
+    # Load model
+    tf.saved_model.loader.load(sess, [vgg_tag], vgg_path)
+    graph = tf.get_default_graph()
+    op = graph.get_operations()
+    [print(m.values()) for m in op][1]
+
+    # Get tensors
+    input = graph.get_tensor_by_name(vgg_input_tensor_name)
+    keep_prob = graph.get_tensor_by_name(vgg_keep_prob_tensor_name)
+    layer3_out = graph.get_tensor_by_name(vgg_layer3_out_tensor_name)
+    layer4_out = graph.get_tensor_by_name(vgg_layer4_out_tensor_name)
+    layer7_out = graph.get_tensor_by_name(vgg_layer7_out_tensor_name)
+
+    print(tf.trainable_variables())
+    
+    return input, keep_prob, layer3_out, layer4_out, layer7_out
 tests.test_load_vgg(load_vgg, tf)
 
 
@@ -46,10 +75,24 @@ def layers(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, num_classes):
     :param num_classes: Number of classes to classify
     :return: The Tensor for the last layer of output
     """
-    # TODO: Implement function
-    return None
-tests.test_layers(layers)
+    # Scale layers 4 and 3 to compensate vgg scale
+    vgg_layer4_out = tf.multiply(vgg_layer4_out, 0.0001, name='vgg_layer4_out')
+    vgg_layer3_out = tf.multiply(vgg_layer3_out, 0.01, name='vgg_layer3_out')
 
+    # Apply transposed convolutions to layers 4 and 7
+    vgg_layer7_out = tf.layers.conv2d_transpose(vgg_layer7_out, num_classes, kernel_size=4, strides=(2, 2), padding='same')
+    vgg_layer7_out = tf.layers.conv2d_transpose(vgg_layer7_out, num_classes, kernel_size=4, strides=(2, 2), padding='same')
+    vgg_layer4_out = tf.layers.conv2d_transpose(vgg_layer4_out, num_classes, kernel_size=4, strides=(2, 2), padding='same')
+    # Sum both layers
+    output = tf.add(vgg_layer7_out, vgg_layer4_out)
+    # Transposed convolution to output and regularizer applied
+    output = tf.layers.conv2d_transpose(output, num_classes, kernel_size=16, strides=(8, 8), padding='same', kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
+    # Transposed convolution to layer 3 and addition to output
+    vgg_layer3_out = tf.layers.conv2d_transpose(vgg_layer3_out, num_classes, kernel_size=8, strides=(8, 8), padding='same')
+    output = tf.add(output, vgg_layer3_out)
+
+    return output
+tests.test_layers(layers)
 
 def optimize(nn_last_layer, correct_label, learning_rate, num_classes):
     """
@@ -60,10 +103,19 @@ def optimize(nn_last_layer, correct_label, learning_rate, num_classes):
     :param num_classes: Number of classes to classify
     :return: Tuple of (logits, train_op, cross_entropy_loss)
     """
-    # TODO: Implement function
-    return None, None, None
-tests.test_optimize(optimize)
+    # Reshape logits and layers
+    logits = tf.reshape(nn_last_layer, (-1, num_classes))
+    labels = tf.reshape(correct_label, (-1, num_classes))
 
+    # Build optimizer and add regularization losses to total loss
+    cross_entropy_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels = labels, logits = logits))
+    optimizer = tf.train.AdamOptimizer(learning_rate = learning_rate)
+    reg_ws = tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES)
+    loss_fn = cross_entropy_loss + tf.reduce_sum(reg_ws)
+    train_op = optimizer.minimize(loss_fn)
+
+    return logits, train_op, loss_fn
+tests.test_optimize(optimize)
 
 def train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_loss, input_image,
              correct_label, keep_prob, learning_rate):
@@ -80,43 +132,70 @@ def train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_l
     :param keep_prob: TF Placeholder for dropout keep probability
     :param learning_rate: TF Placeholder for learning rate
     """
-    # TODO: Implement function
+    # Initialization of variables
+    sess.run(tf.global_variables_initializer())
+    sess.run(tf.local_variables_initializer())
+
+    # Training
+    for epoch in range(epochs):
+        for image, label in get_batches_fn(batch_size):
+            [tr_operation, cr_entropy_loss] = sess.run([train_op, cross_entropy_loss], feed_dict={input_image: image, correct_label: label, keep_prob:0.5, learning_rate:0.00075})
+        print("EPOCH {} ...".format(epoch+1))
+        print("Loss: {}".format(cr_entropy_loss))
     pass
 tests.test_train_nn(train_nn)
 
+def detect_video(img):
+    # Apply logits to video images and apply calculated mask to each of them
+    image = scipy.misc.imresize(img, image_shape)
+    im_softmax = sess.run([tf.nn.softmax(logits)], {keep_prob:1.0, input_image:[image]})
+    im_softmax = im_softmax[0][:,1].reshape(image_shape[0], image_shape[1])
+    segmentation = (im_softmax > 0.5).reshape(image_shape[0], image_shape[1], 1)
+    mask = np.dot(segmentation, np.array([[0,255,0,127]]))
+    mask = scipy.misc.toimage(mask, mode="RGBA")
+    street_im = scipy.misc.toimage(image)
+    street_im.paste(mask, box=None, mask=mask)
+    return np.array(street_im)
 
 def run():
     num_classes = 2
-    image_shape = (160, 576)
     data_dir = './data'
     runs_dir = './runs'
     tests.test_for_kitti_dataset(data_dir)
 
+    epochs = 1#15
+    batch_size = 1#20
+    learning_rate = tf.placeholder(tf.float32)
+    correct_label = tf.placeholder(tf.float32, (None, None, None, num_classes))
+
     # Download pretrained vgg model
     helper.maybe_download_pretrained_vgg(data_dir)
 
-    # OPTIONAL: Train and Inference on the cityscapes dataset instead of the Kitti dataset.
-    # You'll need a GPU with at least 10 teraFLOPS to train on.
-    #  https://www.cityscapes-dataset.com/
+    # Path to vgg model
+    vgg_path = os.path.join(data_dir, 'vgg')
+    # Create function to get batches
+    get_batches_fn = helper.gen_batch_function(os.path.join(data_dir, 'data_road/training'), image_shape)
 
-    with tf.Session() as sess:
-        # Path to vgg model
-        vgg_path = os.path.join(data_dir, 'vgg')
-        # Create function to get batches
-        get_batches_fn = helper.gen_batch_function(os.path.join(data_dir, 'data_road/training'), image_shape)
+    # Build NN using load_vgg, layers, and optimize function
+    global input_image, keep_prob
+    input_image, keep_prob, layer3_out, layer4_out, layer7_out = load_vgg(sess, vgg_path)
+    output_layer = layers(layer3_out, layer4_out, layer7_out, num_classes)
+    global logits
+    logits, train_op, cross_entropy_loss = optimize(output_layer, correct_label, learning_rate, num_classes)
 
-        # OPTIONAL: Augment Images for better results
-        #  https://datascience.stackexchange.com/questions/5224/how-to-prepare-augment-images-for-neural-network
+    # Train NN using the train_nn function
+    train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_loss, input_image, correct_label, keep_prob, learning_rate)
 
-        # TODO: Build NN using load_vgg, layers, and optimize function
+    # Save inference data using helper.save_inference_samples
+    helper.save_inference_samples(runs_dir, data_dir, sess, image_shape, logits, keep_prob, input_image)
 
-        # TODO: Train NN using the train_nn function
+    # Apply the trained model to a video
+    project_output = 'project_output.mp4'
+    clip1 = VideoFileClip('project_video.mp4')
+    project_clip = clip1.fl_image(detect_video)
+    project_clip.write_videofile(project_output, audio=False)
 
-        # TODO: Save inference data using helper.save_inference_samples
-        #  helper.save_inference_samples(runs_dir, data_dir, sess, image_shape, logits, keep_prob, input_image)
-
-        # OPTIONAL: Apply the trained model to a video
-
+    sess.close()
 
 if __name__ == '__main__':
     run()
---
+++ writeup.md
@@ -0,0 +1,89 @@
+# **Semantic Segmentation**
+
+---
+
+**Semantic Segmentation Project**
+
+The goals / steps of this project are the following:
+
+* Detect road pixels of individual frames taken from an automotive video.
+* Use a Fully Convolutiontal Network, so that the spatial information is preserved.
+
+[//]: # (Image References)
+
+[image1]: ./image_output/umm_000013.png "Image detection"
+[image2]: ./output_images/undistort_test_image.png "Road Transformed"
+[image3]: ./output_images/threshold.png "Binary Example"
+[image4]: ./output_images/warped.png "Warp Example"
+[image5]: ./output_images/lane_detection.png "Fit Visual"
+[image6]: ./output_images/view.png "Output"
+[video1]: ./project_video.mp4 "Video"
+
+## Rubric Points
+
+Here I will consider the [rubric points](https://review.udacity.com/#!/rubrics/989/view) individually and describe how I addressed each point in my implementation.  
+
+---
+
+### Writeup / README
+
+### Build the neural network
+
+#### 1. Does the project load the pretrained vgg model?
+
+The pretrained vgg model is loaded in the function `load_vgg`, via `load` Tensorflow function. Additionally, the `input_image` and `keep_prob` placeholders as well as the tensors `layer3_out`, `layer4_out` and `layer7_out` are extracted from the pretrained model for creating a fully convolutional network and building skip connections according to the `FCN-8s` architecture.
+
+#### 2. Does the project learn the correct features from the images?
+
+The pretrained model extracts the features from the images with the original configuration, since the feature layers and the `1x1` convolutions are already defined in the provided network. The fully convolutional network is built in the function `layers` via application of transposed convolutions to `layer7_out` and skip connections with `layer4_out` and `layer3_out`.
+
+Two transposed convolutions of `kernel_size=4` and `strides=2` are applied to `layer7_out` and one to `layer4_out` before connecting them. A transposed convolution of `kernel_size=16` and `strides=8` is applied to the result, as well as to `layer3_out`. The sum of both represents the final output tensor (logits), which has the shape of the original image and where the pixels are correctly classified thanks to a successful feature learn.
+
+#### 3. Does the project optimize the neural network?
+
+The function `optimize` implements the optimization operations and calculates the loss. An adam optimizer is used to minimize it and the total loss also includes the loss obtained via L2 regularization with scale of `1e-3`, applied to the last transposed convolution.
+
+#### 4. Does the project train the neural network?
+
+The function `train_nn` performs the training. The training tensor defined in the `optimize` function is evaluated, as well as the loss one. The evaluation is done for `epochs=15` and taking `batch_size=20`. A `keep_prob` of `0.5` is defined for the dropout layers of the vgg, as well as a `learning_rate` of `0.00075`. The loss is monitored while the training is performed.
+
+### Neural network training
+
+#### 5. Does the project train the model correctly?
+
+The model decreases the loss on average for each epoch. For the training corresponding to the attached image and video results the following loss was obtained:
+
+| Epoch         		|     Loss	        					|
+|:---------------------:|:---------------------------------------------:|
+| 1         		| 0.47   							|
+| 2     	| 0.40 	|
+| 3					|	0.39											|
+| 4	      	| 0.36 				|
+|	5					|	0.34											|
+| 6	    | 0.37     									|
+| 7					|	0.30											|
+| 8	      	| 0.25 				|
+|	9					|	0.24											|
+| 10		| 0.18        									|
+| 11		| 0.19       									|
+| 12				| 0.18        									|
+|	13					|	0.19											|
+| 14		| 0.18        									|
+| 15		| 0.16       									|
+
+
+
+
+#### 6. Does the project use reasonable hyperparameters?
+
+As mentioned in previous sections, the evaluation is done for `epochs=15` and taking `batch_size=20`. The mentioned number of epochs was selected because it was the limit number by which the loss stops being reduced. The mentioned batch size ensured that the used GPU does not run out of memory. Both values ensured also a good detection of both the image test set and the video.
+
+#### 7. Does the project correctly label the road?
+
+Most pixels are labeled close to the best solutions. The following image is an example of the detection:
+
+![alt text][image1]
+
+Additionally the logits were applied to a random video (`project_video.mp4`). The result is shown in the following link (click on image):
+
+[![Alt text for your video](https://i.ytimg.com/vi/5nY5wKeFI98/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLBR9UpzzHVJjRvFQvwUOtv8E6dIXA)](https://www.youtube.com/watch?v=5nY5wKeFI98)
